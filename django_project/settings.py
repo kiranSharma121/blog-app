509d182638eebfb69f290d96c08c46307980f317
kiranSharma121 <poudelkiran.np@gmail.com>
2026-01-12 23:25:24 +0530

    feat:making somechanges

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-from dotenv import load_dotenv
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,7 +26,7 @@ SECRET_KEY = 'django-insecure-1ex_gq0^k#dz9m%-(&(gwxeo8xu+*=flz148i%&1b)&*l3#jzq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
