8b2136b2ee0c338bd57f727ced6fc845a1bea816
kiranSharma121 <124862735+kiranSharma121@users.noreply.github.com>
2026-01-13 00:58:20 +0530

    Update urls.py

--- django_project/urls.py
+++ django_project/urls.py
@@ -13,7 +13,8 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
-   path('', include('blog.urls')),
+   path('', include('django_project.blog.urls')),
+
 ]
 
 if settings.DEBUG:
