a50b9c8c6c6c36ceb75f9b566140f8c8797d8051
kiranSharma121 <poudelkiran.np@gmail.com>
2026-01-16 00:26:33 +0530

    fixing some bugs

--- users/models.py
+++ users/models.py
@@ -7,33 +7,35 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(
         upload_to='profile_pics',
-        default='profile_pics/avatar_default.jpg'
+        default='profile_pics/default.jpg'
     )
 
-def save(self, *args, **kwargs):
-    super().save(*args, **kwargs)
+    def __str__(self):
+        return self.user.username
 
-    if self.image.name == 'profile_pics/avatar_default.jpg':
-        return
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
 
-    if not os.path.isfile(self.image.path):
-        return
+        if self.image.name == 'profile_pics/default.jpg':
+            return
 
-    img = Image.open(self.image.path)
+        if not os.path.isfile(self.image.path):
+            return
 
+        img = Image.open(self.image.path)
 
-    if img.mode in ("RGBA", "P"):
-        img = img.convert("RGB")
+        if img.mode in ("RGBA", "P"):
+            img = img.convert("RGB")
 
-    width, height = img.size
-    min_side = min(width, height)
+        width, height = img.size
+        min_side = min(width, height)
 
-    left = (width - min_side) / 2
-    top = (height - min_side) / 2
-    right = (width + min_side) / 2
-    bottom = (height + min_side) / 2
+        left = (width - min_side) / 2
+        top = (height - min_side) / 2
+        right = (width + min_side) / 2
+        bottom = (height + min_side) / 2
 
-    img = img.crop((left, top, right, bottom))
-    img = img.resize((300, 300), Image.LANCZOS)
+        img = img.crop((left, top, right, bottom))
+        img = img.resize((300, 300), Image.LANCZOS)
 
-    img.save(self.image.path, format="JPEG", quality=90)
+        img.save(self.image.path, format="JPEG", quality=90)
