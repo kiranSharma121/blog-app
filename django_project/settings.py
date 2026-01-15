b341ac96bac1941a7806919f5ff0e7ea327764d6
kiranSharma121 <poudelkiran.np@gmail.com>
2026-01-16 00:56:24 +0530

    feat:connecting to database

--- django_project/settings.py
+++ django_project/settings.py
@@ -82,12 +82,16 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+import dj_database_url
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
+
 
 
 # Password validation
